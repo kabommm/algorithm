840af78f9ec2e134dded10af273a43b8bfabf52a
김동선 (Kim DongSun) <103254904+kabommm@users.noreply.github.com>
2023-06-13 14:31:44 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.02 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.java
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -1,11 +1,11 @@
 class Solution {
     public int solution(int[] arr) {
-        int answer = arr[0];
+        int answer = 0;
         int g = 0;
         if(arr.length == 1){    //길이가 1인 경우
             return arr[0];  
         }
-        // answer = arr[0];
+        answer = arr[0];
         
         for(int i = 1; i < arr.length; i++) {
             g = gcd(answer, arr[i]);    //최대 공약수
