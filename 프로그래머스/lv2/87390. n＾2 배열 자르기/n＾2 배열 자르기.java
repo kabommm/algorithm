58c894d793e707b559d2167395697118e5fa86af
김동선 (Kim DongSun) <103254904+kabommm@users.noreply.github.com>
2023-06-19 18:56:18 +0900

    [level 2] Title: n^2 배열 자르기, Time: 9.30 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
+++ 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
@@ -8,7 +8,7 @@ class Solution {
         // 3 3 3    arr[2][0] = 3 arr[2][1] = 3 arr[2][2] = 3
 
         for(long i=left; i<right+1; i++){
-             al.add(Math.max(i/n,i%n) + 1);
+             al.add(Math.max(i / n,i % n) + 1);
         }
         return al;
     }
