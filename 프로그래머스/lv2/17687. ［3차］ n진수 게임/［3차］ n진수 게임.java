88ae0f28180f4e439fccfb20be050f29d018addc
김동선 (Kim DongSun) <103254904+kabommm@users.noreply.github.com>
2023-06-21 22:35:07 +0900

    [level 2] Title: [3차] n진수 게임, Time: 32.23 ms, Memory: 97.6 MB -BaekjoonHub

--- 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -1,19 +1,24 @@
 class Solution {
     public String solution(int n, int t, int m, int p) {
-        //진법 n, 미리 구할 숫자의 갯수 t, 게임에 참가하는 인원 m, 튜브의 순서 p
-        StringBuilder sb = new StringBuilder();
-        StringBuilder answer = new StringBuilder();
         
-        // t * m 즉 몇바퀴, 0부터 계속 n진법으로 변환한 것을 저장
-        for(int i = 0; sb.length() <= t * m; i++){
-            sb.append(Integer.toString(i, n));
-        }
+    String answer = "";
+    int num = 0;
+    String str = "";
+    String mystr = "";
+
+    // (t * m)t바퀴동안, 0부터 계속 n진법으로 변환한 것을 저장
+    while (str.length() < m * t) {  
+        str += Integer.toString(num, n);
+        num++;
+    }
+    // t회, 내 차례만 정답에 추가
+    for (int i=0; i<t; i++) {
+        mystr += str.charAt(p - 1 + i * m);
+    }
+    //대문자 변환
+    answer = mystr.toUpperCase();
         
-        // 총 t회, 내 차례만 정답에 추가
-        for(int i = p - 1; answer.length() < t; i += m){
-            answer.append(sb.charAt(i));
-        }
-        // StringBuilder 문자열로 변환 + 대문자 변환
-        return answer.toString().toUpperCase();
+    return answer.toString().toUpperCase();
+        
     }
 }
