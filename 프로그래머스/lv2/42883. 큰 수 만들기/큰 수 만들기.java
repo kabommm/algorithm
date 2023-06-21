fd0d5036e9beefd0d0b937669322c173814f34d4
김동선 (Kim DongSun) <103254904+kabommm@users.noreply.github.com>
2023-06-21 23:28:13 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.23 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,24 +1,22 @@
 import java.util.*;
 class Solution {
     public String solution(String number, int k) {
-        //숫자를 두개 제거하면 만들 수 있는 수를 배열에 담고
-        //오름차순 뒤 맨 마지막 원소의 값 반환
-        String answer = "";
-        StringBuilder sb = new StringBuilder();
-        int idx = 0;
-        int max = 0;
         
-        for(int i=0; i<number.length() - k; i++) {
-            max = 0;
-            for(int j = idx; j <= k+i; j++) {
-                if(max < number.charAt(j)-'0') {
-                    max = number.charAt(j)-'0';
-                    idx = j+1;
-                }
+        char[] result = new char[number.length() - k];
+        Stack<Character> stack = new Stack<>();
+
+        for(int i=0; i<number.length(); i++) {
+            char c = number.charAt(i);
+            //스택이 비어있지 않고 등장한 문자가 크면서 k--가 0보다 크면
+            while (!stack.isEmpty() && stack.peek() < c && k-- > 0) {
+                stack.pop();
             }
-            sb.append(max);
+            stack.push(c);
         }
-        answer = sb.toString();
-        return answer;
+        for (int i=0; i<result.length; i++) {
+            result[i] = stack.get(i);
+        }
+        
+        return new String(result);
     }
 }
