92cc21d09f6a59fcb9556a7924b1c1b485a657a7
김동선 (Kim DongSun) <103254904+kabommm@users.noreply.github.com>
2023-06-29 22:37:11 +0900

    Update 나이순 정렬.java

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -13,8 +13,8 @@ public class Main {
             arr[i][0] = age;
             arr[i][1] = name;
         }
-        //이름 오름차순
-        Arrays.sort(arr, (s1, s2) -> Integer.parseInt(s1[0]) - Integer.parseInt(s2[0]) );
+        //단순 배열의 원소가 아닌 배열의 특정 부분(이름)에 따라 정렬해야 하므로 오름차순 식을 사용한다(람다식 표현)
+        Arrays.sort(arr, (s1, s2) -> Integer.parseInt(s1[0]) - Integer.parseInt(s2[0]) );    
        
         for(int i=0; i<N; i++){
             System.out.println(arr[i][0] + " " + arr[i][1]);
