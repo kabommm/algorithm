db51a093695410d88d88d83912ad59d7df4627e5
김동선 (Kim DongSun) <103254904+kabommm@users.noreply.github.com>
2023-06-29 12:21:55 +0900

    [level 2] Title: 124 나라의 숫자, Time: 1.58 ms, Memory: 51.9 MB -BaekjoonHub

--- 프로그래머스/lv2/12899. 124 나라의 숫자/124 나라의 숫자.java
+++ 프로그래머스/lv2/12899. 124 나라의 숫자/124 나라의 숫자.java
@@ -1,16 +1,11 @@
 class Solution {
     public String solution(int n) {
         String answer = "";
-        String[] numbers = {"4", "1", "2"}; //규칙성을 위해 배열을 이용.
-      
+        String[] num = {"4", "1", "2"}; //규칙성을 위해 배열을 이용.
+        
         while(n > 0){
-          int idx = n % 3;  //나머지가 0이면 4, 1이면 1, 2이면 2
-          n /= 3;
-          
-          if(idx == 0){
-            --n;
-          }
-          answer = numbers[idx] + answer;
+          answer = num[n % 3] + answer; //나머지가 0이면 4, 1이면 1, 2이면 2
+          n = (n - 1) / 3;
         }
         return answer;
     }
