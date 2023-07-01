31fcab3e57a8019d446d08345a4cd5e495b6bdae
김동선 (Kim DongSun) <103254904+kabommm@users.noreply.github.com>
2023-07-01 22:12:09 +0900

    [Silver IV] Title: 베스트셀러, Time: 236 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n  = sc.nextInt(); // 오늘 하루 동안 팔린 책의 개수
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for(int i=0; i<n; i++){
+            String title = sc.next();
+            map.put(title, map.getOrDefault(title,0)+1);
+        }
+        String maxTitle = "";
+        int max = 0;
+        //value 값의 비교가 필요하기 때문에 getValue()를 사용할 수 있는 entrySet 를 사용
+        for (HashMap.Entry<String, Integer> entry : map.entrySet()) {
+            // value 가 최대 값이거나(가장 많이 팔린)
+            // 최대 값이 같으면 Title(책 이름) 사전순
+            if(entry.getValue() > max || (entry.getValue() == max &&
+                    entry.getKey().compareTo(maxTitle) < 0)){
+                maxTitle = entry.getKey();
+                max = entry.getValue();
+            }
+        }
+        System.out.println(maxTitle);
+    }
+}
