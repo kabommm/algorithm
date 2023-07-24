7d7301e7c2e9be1241d0f6169f56ffacba875837
김동선 (Kim DongSun) <103254904+kabommm@users.noreply.github.com>
2023-07-24 15:18:08 +0900

    Update ［3차］ 파일명 정렬.java

--- 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
+++ 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -5,16 +5,14 @@ class Solution {
         Arrays.sort(answer, (o1, o2) -> {
         String head1 = o1.split("[0-9]")[0].toLowerCase();
         String head2 = o2.split("[0-9]")[0].toLowerCase();
+        int result = head1.compareTo(head2);    //숫자 앞 HEAD의 사전순 정렬
 
-        int result = head1.compareTo(head2);
-
-        if (result == 0) {
-            int number1 = convert(o1, head1);
-            int number2 = convert(o2, head2);
+        if(result == 0){    //HEAD가 같은 문자면 -> 숫자 비교
+            int number1 = convert(o1, head1);   //NUMBER 추출
+            int number2 = convert(o2, head2);   //NUMBER 추출
 
             result = Integer.compare(number1, number2);
         }
-
         return result;
     });
         return answer;
@@ -24,13 +22,12 @@ class Solution {
         StringBuilder number = new StringBuilder();
         s = s.substring(head.length());
         for (char c : s.toCharArray()) {
-            if (c >= '0' && c <= '9') {
-                number.append(c);
-            } else {
+            if (c >= '0' && c <= '9') { //숫자이면
+                number.append(c);   //숫자를 순서대로 이어붙히기 
+            }else{  //숫자가 끝나면 종료
                 break;
             }
         }
-
-        return Integer.parseInt(number.toString());
+        return Integer.parseInt(number.toString()); //이어붙힌 숫자를 int형으로
     }
 }
