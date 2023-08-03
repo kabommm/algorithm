5756e9dc8010e0ae55f8362a09c5122e5ede33f8
김동선 (Kim DongSun) <103254904+kabommm@users.noreply.github.com>
2023-08-03 15:41:49 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/lv3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,8 +1,8 @@
 -- 코드를 입력하세요
 # 테이블이 두개다? JOIN
-SELECT animal_in.ANIMAL_ID, animal_in.NAME
+SELECT animal_in.ANIMAL_ID, animal_in.NAME # 동물의 아이디와 이름을 조회
 FROM ANIMAL_INS AS animal_in
-INNER JOIN ANIMAL_OUTS AS animal_out
+INNER JOIN ANIMAL_OUTS AS animal_out  # 두 테이블의 컬럼이 모두 같으므로 INNER JOIN
 ON animal_in.ANIMAL_ID = animal_out.ANIMAL_ID 
-WHERE animal_out.DATETIME < animal_in.DATETIME
+WHERE animal_out.DATETIME < animal_in.DATETIME # 보호 시작일보다 입양일이 더 빠른
 ORDER BY animal_in.DATETIME; # 보호 시작일이 빠른 순으로 조회
