c4cb7982242449c9be0bbc16452e82d7db490b05
김동선 (Kim DongSun) <103254904+kabommm@users.noreply.github.com>
2023-08-04 19:06:59 +0900

    [level 3] Title: 여행경로, Time: 12.93 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/lv3/43164. 여행경로/여행경로.java
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.java
@@ -2,14 +2,15 @@ import java.util.*;
 class Solution {
     
     boolean[] visited;
-    ArrayList<String> allRoute; 
+    ArrayList<String> list; 
     
     public void dfs(String start, String route, String[][] tickets, int cnt){
         if(cnt == tickets.length){
-            allRoute.add(route);
+            list.add(route);
             return;
         }
-        
+        //ICN SFO ATL ICN ATL SFO
+        //visited[0], visited[2] visited[3] visited[1] visited[4] 
         for(int i=0; i<tickets.length; i++){
             if(start.equals(tickets[i][0]) && !visited[i]){
                 visited[i] = true;
@@ -22,12 +23,12 @@ class Solution {
         String[] answer = {};
         int cnt = 0;
         visited = new boolean[tickets.length];
-        allRoute = new ArrayList<>();
+        list = new ArrayList<>();
         
         dfs("ICN", "ICN", tickets, cnt);
-        
-        Collections.sort(allRoute);
-        answer = allRoute.get(0).split(" ");
+        System.out.println(list.get(0));
+        Collections.sort(list); //정렬 이해가 안감
+        answer = list.get(0).split(" ");
         return answer;
     }
 }
