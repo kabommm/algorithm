8b716e4d46376462ba10947b2eb505999984fe86
김동선 (Kim DongSun) <103254904+kabommm@users.noreply.github.com>
2023-08-08 18:00:39 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 1.48 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -6,10 +6,9 @@ class Solution {
     static ArrayList<Integer>[] graph;
     static int min;
     
-    //함수
     static int dfs(int v, boolean[] visited) {
         visited[v] = true;
-        int cnt = 1;
+        int cnt = 1;    //노드 개수를 세는 변수
  
         for (int next : graph[v]) {
             if (!visited[next]) {
@@ -22,34 +21,35 @@ class Solution {
     
     //메인 함수
     public int solution(int n, int[][] wires) {
-        graph = new ArrayList[n + 1];
-        min = Integer.MAX_VALUE;
+        graph = new ArrayList[n + 1];   //그래프의 인접 리스트를 저장하는 배열
+        min = Integer.MAX_VALUE;    //트리의 최소 크기
  
-        // 그래프 ArrayList 초기화. 노드 개수만큼 ArrayList 생성
+        // 그래프 ArrayList 초기화
         for (int i = 1; i <= n; i++) {
             graph[i] = new ArrayList<>();
         }
  
-        // 양방향 간선 구조이므로 두 번 add를 해준다
+        // 양방향 간선 추가
         for (int i = 0; i < wires.length; i++) {
             int v1 = wires[i][0];
             int v2 = wires[i][1];
             graph[v1].add(v2);
             graph[v2].add(v1);
         }
- 
+        
         for (int i = 0; i < wires.length; i++) {
             int v1 = wires[i][0];
             int v2 = wires[i][1];
- 
+            
             boolean[] visited = new boolean[n + 1];
- 
+
             // 해당 간선을 그래프에서 제거
+            //Arraylist에서 Integer객체를 제거해야 하므로 int -> Integer로 변환
             graph[v1].remove(Integer.valueOf(v2));
             graph[v2].remove(Integer.valueOf(v1));
  
             int cnt = dfs(1, visited); // 임의의 시작점에서 dfs 탐색
- 
+            // 그룹의 노드 개수 차이를 구하고, 최소 트리 크기인 min을 갱신
             int diff = Math.abs(cnt - (n - cnt));
             min = Math.min(min, diff);
  
