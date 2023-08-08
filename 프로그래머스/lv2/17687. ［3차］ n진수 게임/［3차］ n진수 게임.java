8580f03b6bfd426941b94d1da574e825946be700
김동선 (Kim DongSun) <103254904+kabommm@users.noreply.github.com>
2023-08-08 14:41:17 +0900

    [level 2] Title: [3차] n진수 게임, Time: 28.28 ms, Memory: 92.1 MB -BaekjoonHub

--- 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -4,21 +4,19 @@ class Solution {
     String answer = "";
     int num = 0;
     String str = "";
-    String mystr = "";
 
     // (t * m)t바퀴동안, 0부터 계속 n진법으로 변환한 것을 저장
-    while (str.length() < m * t) {  
+    while (str.length() < m * t) {  //총 m * t +1번
         str += Integer.toString(num, n);
         num++;
     }
     // t회, 내 차례만 정답에 추가
     for (int i=0; i<t; i++) {
-        mystr += str.charAt(p - 1 + i * m);
+        answer += str.charAt(p - 1 + i * m);
     }
     //대문자 변환
-    answer = mystr.toUpperCase();
         
-    return answer.toString().toUpperCase();
+    return answer.toUpperCase();
         
     }
 }
