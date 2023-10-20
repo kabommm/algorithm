ad683583a84e6fe3a431410d5c1e052787c6e54f
김동선 (Kim DongSun) <103254904+kabommm@users.noreply.github.com>
2023-10-20 21:52:28 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 7.95 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] solution(String s) {
         int[] answer = new int[2];  //[반복 횟수,제거한 0의 수]
+        
         while(!s.equals("1")) {  //s가 1이될 때까지 반복
             int count = 0;
             for(int i=0; i<s.length(); i++){
@@ -9,8 +10,7 @@ class Solution {
                     count++;    //0이 나온 수
                 }
             }
-            s = Integer.toString(s.length() - count,2);    //이진으로 변환(문자열)
-            //s = Integer.toBinaryString(len);
+            s = Integer.toString(s.length() - count,2);  //이진으로 변환(문자열)
             //Integer.toString(8,2)   (변활할 숫자,N진수)
             answer[0]++;    //한 번 순회할 때마다 배열의 첫번째 원소 증가
         }
