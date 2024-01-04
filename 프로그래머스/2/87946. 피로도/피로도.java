fe7a5a920689210de86e3e187c295d5b329f9fbe
김동선 (Kim DongSun) <103254904+kabommm@users.noreply.github.com>
2024-01-04 15:26:40 +0900

    [level 2] Title: 피로도, Time: 0.08 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -2,16 +2,17 @@ class Solution {
     
     //변수 선언
     static boolean[] visited;   //방문배열  
-    static int answer = 0;  //답
+    static int answer;  //답
+    
     //dfs함수
-    public void dfs(int depth, int fatigue, int[][] dungeons){  
+    public void dfs(int depth, int limit, int[][] dungeons){  
         for (int i = 0; i < dungeons.length; i++){  
             //방문하지 않았거나 피로도가 남았다면
-            if (!visited[i] && dungeons[i][0] <= fatigue) {  
+            if (!visited[i] && dungeons[i][0] <= limit) {  
                 visited[i] = true;  //방문처리  
-                dfs(depth + 1, fatigue - dungeons[i][1], dungeons);    //재귀!
+                dfs(depth + 1, limit - dungeons[i][1], dungeons);
                 visited[i] = false;  
-            }//그렇지 않으면  
+            }
              
         }  
         answer = Math.max(answer, depth);  
@@ -19,6 +20,7 @@ class Solution {
     //메인 함수
     public int solution(int k, int[][] dungeons) {  //[최소 피로도,소모 피로도]
         visited = new boolean[dungeons.length];  
+        
         dfs(0, k, dungeons);    //시작점, 입력받은 k, 입력받은 배열
         return answer;
     }
