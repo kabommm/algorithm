ed3c8d4d45029f4a6b49547fd01f84bfb26e4801
김동선 (Kim DongSun) <103254904+kabommm@users.noreply.github.com>
2024-03-27 18:37:57 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 67.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -4,7 +4,7 @@ class Solution {
         //규칙: answer[0] * answer[1] = brown + yellow
         int sum = brown+yellow;
         for(int i=3; i<=sum; i++){  //가로는 최소3
-            if(sum % i == 0 && sum/i >= 3){//sum이 i로 나누어 떨어지고 세로 최소3
+            if(sum % i == 0){
                 int col = i;
                 int row = sum/i;
                 //가로가 세로보다 같거나 크며 노란 격자가 가운데 존재하기 위해선 (가로 - 2) * (세로 - 2 ) = yellow
