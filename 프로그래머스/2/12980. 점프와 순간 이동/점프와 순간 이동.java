f0126235308c6ecdcf30b9cf321e08d4e41fc9fc
김동선 (Kim DongSun) <103254904+kabommm@users.noreply.github.com>
2024-04-02 18:49:14 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.03 ms, Memory: 52.3 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -3,10 +3,6 @@ import java.util.*;
 public class Solution {
     public int solution(int n) {
         int ans = 0;
-        int jump = 0;   //점프 횟수
-        int sum = 1;    //현 위치, 0은 2를 곱해도 0이므로 1로 시작 대신 배터리 1소모
-        // +k or 지금까지 온 거리 sum * 2로 이동
-        //점프+순간이동으로 도달 가능한 경우의 수를 구하고 그중 최솟값 반환
         
         while(true){    //값이 반환 될 때까지 계속 반복
             if(n == 0){ //역추적한 n이 시작점으로 돌아갔을때 비로소 탈출 종료
